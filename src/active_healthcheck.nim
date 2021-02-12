17b7e2bf9350d5f03ecd8df9a4594d82965bcd9d
salvador <mehdi73ee@gmail.com>
2021-02-12 23:26:14 +0330

    loop over hosts array and checking for connection

--- src/active_healthcheck.nim
+++ src/active_healthcheck.nim
@@ -1,11 +1,16 @@
+import strutils
 import asyncnet, asyncdispatch
 
 
-proc start_health_check*(socket: AsyncSocket) {.async.} =
+proc start_health_check*(socket: AsyncSocket, hosts: seq[string]) {.async.} =
+    echo(hosts)
+
     echo("connecting ...")
-    try:
-        await socket.connect("127.0.0.1", 5001.Port)
-        echo("connected.")
-    except OSError:
-        echo("not connected")
+    for host in hosts:
+        let host_port = host.split(":")
+        try:
+            await socket.connect(host_port[0], host_port[1].parseInt.Port)
+            echo("connected.")
+        except OSError:
+            echo("not connected")
     # asyncCheck socket.send("message" & "\c\l")
