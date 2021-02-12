41a5c1eabdb78c2994f239067377f0d2e02fcc95
salvador <mehdi73ee@gmail.com>
2021-02-12 23:25:45 +0330

    added a test hosts array

--- src/loadbalancer.nim
+++ src/loadbalancer.nim
@@ -3,9 +3,7 @@ import active_healthcheck
 # import threadpool
 import asyncnet, asyncdispatch
 
-
+var hosts: seq[string] = @["127.0.0.1:8000", "127.0.0.1:8001"]
 var c_socket = newAsyncSocket()
-asyncCheck start_health_check(c_socket)
-# asyncCheck c_socket.send("message" & "\c\l")
-# runForever()
+asyncCheck start_health_check(c_socket, hosts)
 asyncdispatch.poll()
