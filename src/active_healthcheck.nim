7bd44d56a5c0bb36c294c2d712c3f1d4c85b259a
salvador <mehdi73ee@gmail.com>
2021-02-21 18:25:00 +0330

    loop for continues health checking

--- src/active_healthcheck.nim
+++ src/active_healthcheck.nim
@@ -3,14 +3,12 @@ import asyncnet, asyncdispatch
 
 
 proc start_health_check*(socket: AsyncSocket, hosts: seq[string]) {.async.} =
-    echo(hosts)
-
     echo("connecting ...")
     for host in hosts:
+        echo(host)
         let host_port = host.split(":")
         try:
             await socket.connect(host_port[0], host_port[1].parseInt.Port)
             echo("connected.")
         except OSError:
             echo("not connected")
-    # asyncCheck socket.send("message" & "\c\l")
